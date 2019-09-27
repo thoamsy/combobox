0fde166321720131076a9d57830ecc36d0967378
Yang Kui <c6swift@gmail.com>
2019-09-27 15:50:48 +0800

    feat: 基本功能完成

--- src/ComboList.jsx
+++ src/ComboList.jsx
@@ -1,18 +1,2 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-
-
-function ComboList({ data }) {
-  return (
-    <ul>
-      {data.map(({ title }) => (
-        <li tabIndex={0} key={title}>
-          {title}
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-export default ComboList;
--- src/Combobox.jsx
+++ src/Combobox.jsx
@@ -1,77 +1,157 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useRef, createContext, useMemo, useContext, useEffect } from 'react';
+import React, {
+  useRef,
+  createContext,
+  useMemo,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from 'react';
+import './style.css';
 
 const Context = createContext({
-  onSelect() { },
-  optionsRef: { current: [] }
-})
+  onSelect() {},
+  optionsRef: { current: [] },
+});
 
-const data = [
-  {
-    title: 'abc',
-    id: 'abc'
-  },
-  {
-    title: 'cd',
-    id: 'cd'
-  }
-]
-
-function Combobox({ onSelect }) {
+function Combobox({ as: Comp = 'div', onSelect, ...props }) {
   const optionsRef = useRef([]);
+  const [selected, setSelected] = useState(null);
 
-  useEffect(() => {
-    console.count('app')
-  })
-  const value = useMemo(() => ({
-    optionsRef,
-    onSelect,
-  }), []);
+  const value = useMemo(
+    () => ({
+      optionsRef,
+      onSelect,
+      selected,
+      setSelected,
+    }),
+    [selected, onSelect],
+  );
 
   return (
     <Context.Provider value={value}>
-      <ComboList data={data} />
+      <Comp {...props} data-combobox="" />
     </Context.Provider>
   );
 }
 Combobox.defaultProps = {
-  onSelect() {
+  onSelect() {},
+};
 
-  }
-}
+function ComboList({ as: C = 'ul', ...props }) {
+  // const { optionsRef } = useContext(Context);
 
-function ComboList({ data }) {
-  const { optionsRef } = useContext(Context)
-
-  useEffect(() => {
-    console.log(optionsRef.current, 'lll')
-  });
+  const handleKeydown = useKeydown();
 
   return (
-    <ul>
-      {data.map(({ title }) => (
-        <ComboOption value={title} key={title}>
-          {title}
-        </ComboOption>
-      ))}
-    </ul>
-  )
+    <C onKeyDown={handleKeydown} {...props} data-combobox-list role="listbox" />
+  );
 }
 
-function ComboOption({ value, children, ...props }) {
-
-  const { optionsRef, onSelect } = useContext(Context)
+function ComboOption({ as: C = 'li', value, ...props }) {
+  const { optionsRef, onSelect, selected, setSelected } = useContext(Context);
   useEffect(() => {
     optionsRef.current.push(value);
   }, [optionsRef, value]);
 
   const onClick = () => {
-    onSelect(value)
-  }
+    setSelected(value);
+    onSelect(value);
+  };
+
+  const isActive = selected === value;
 
   return (
-    <li {...props} role="option" tabIndex={0} onClick={onClick}>{children}</li>
-  )
+    <C
+      data-combobox-option
+      data-highlighted={isActive ? '' : undefined}
+      {...props}
+      tabIndex={-1}
+      onClick={onClick}
+    />
+  );
 }
 
-export default Combobox;
+function useKeydown() {
+  const { onSelect, optionsRef, selected, setSelected } = useContext(Context);
+
+  return function handleKeydown(event) {
+    const { current: options } = optionsRef;
+
+    switch (event.key) {
+      case 'ArrowDown': {
+        event.preventDefault();
+
+        if (!options || options.length === 0) {
+          return;
+        }
+
+        const pos = options.indexOf(selected);
+
+        setSelected(options[(pos + 1) % options.length]);
+        break;
+      }
+      case 'ArrowUp': {
+        event.preventDefault();
+
+        if (!options || options.length === 0) {
+          return;
+        }
+
+        const pos = options.indexOf(selected);
+
+        setSelected(options[(pos - 1 + options.length) % options.length]);
+        break;
+      }
+      case 'Enter': {
+        event.preventDefault();
+        onSelect && onSelect(selected);
+        break;
+      }
+      default:
+        break;
+    }
+  };
+}
+
+const App = () => {
+  const [data, setData] = useState(() => [
+    {
+      title: 'abc',
+      id: 'abc',
+      isCheck: false,
+    },
+    {
+      title: 'cd',
+      id: 'cd',
+      isCheck: false,
+    },
+  ]);
+
+  const onSelect = useCallback(
+    value => {
+      setData(data => {
+        return data.map(item =>
+          item.title === value ? { ...item, isCheck: !item.isCheck } : item,
+        );
+      });
+    },
+    [data],
+  );
+
+  console.log(data);
+  return (
+    <Combobox onSelect={onSelect}>
+      <ComboList>
+        {data.map(({ title, isCheck }) => (
+          <ComboOption value={title} key={title} type="checkbox">
+            <input type="checkbox" onChange={() => {}} checked={isCheck} />{' '}
+            {title}
+          </ComboOption>
+        ))}
+      </ComboList>
+    </Combobox>
+  );
+};
+export default App;
